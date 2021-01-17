b2ac6c67dbebdc38c2ad183c138f8d26501265fc
alokbhargav <74970258+alokbhargav@users.noreply.github.com>
2021-01-17 08:03:21 +0530

    Add files via upload

---
+++ IFileOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataApplication
+{
+    public interface IFileOperation
+    {
+        string[] GetFileContent();
+        List<string> GetDriverName(string[] rawData);
+        List<TripDetailModel> GetTripDetail(string[] rawData);
+    }
+}
---
+++ TripDetailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace KataApplication
+{
+    public class TripDetailModel 
+    {
+        public string DriverName { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public double Distance { get; set; }
+
+        
+    }
+}
